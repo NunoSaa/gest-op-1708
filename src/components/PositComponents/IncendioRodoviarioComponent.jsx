03fb6f3a4e9c62ab71a6d5a30bcb562130b06453
Nuno Santos <nuno.90.santos@gmail.com>
2024-10-16 21:37:52 +0100

    Update IncendioRodoviarioComponent.jsx

--- src/components/PositComponents/IncendioRodoviarioComponent.jsx
+++ src/components/PositComponents/IncendioRodoviarioComponent.jsx
@@ -4,18 +4,144 @@ import { TextField, FormGroup, FormControlLabel, Checkbox, Card, CardContent, fo
 const IncendiosRodoviariosComponent = ({
     pontosSituacao,
     handleCheckboxChange,
-    fogoVista,
-    handleFogoVistaChange,
-    em,
-    handleEmChange,
+    emVeiculo,
+    handleEmVeiculoChange,
     geolocationGMS,
     estouEm,
     setEstouEm,
+    tipoCarga,
+    handleTipoCargaChange,
     localidade,
     handleChange,
 }) => {
     return (
-<span style={styles.infoProp}>Incendios rodoviarios</span>
+        <div>
+            <div style={styles.rowInfo}>
+                <h2>ESTOU: </h2>
+                <span style={styles.infoProp}>Estou em: </span>
+                <TextField
+                    style={styles.input}
+                    type="text"
+                    name="estou"
+                    value={estouEm}
+                    onChange={(e) => setEstouEm(e.target.value)}
+                    variant="outlined"
+                    fullWidth
+                />
+            </div>
+
+            <div style={styles.rowInfo}>
+                <span style={styles.infoProp}>Latitude N: </span>
+                <TextField
+                    style={styles.input}
+                    type="text"
+                    name="localidade"
+                    value={geolocationGMS.latitude}
+                    disabled
+                    variant="outlined"
+                    fullWidth
+                />
+            </div>
+
+            <div style={styles.rowInfo}>
+                <span style={styles.infoProp}>Longitude W: </span>
+                <TextField
+                    style={styles.input}
+                    type="text"
+                    name="localidade"
+                    value={geolocationGMS.longitude}
+                    disabled
+                    variant="outlined"
+                    fullWidth
+                />
+            </div>
+
+            <div style={styles.rowInfo}>
+                <span style={styles.infoProp}>Freguesia / Município: </span>
+                <TextField
+                    style={styles.input}
+                    type="text"
+                    name="localidade"
+                    value={localidade}
+                    onChange={handleChange}
+                    variant="outlined"
+                    fullWidth
+                />
+            </div>
+
+            <h2>VEJO: </h2>
+
+            <div style={styles.rowInfo}>
+                <span style={styles.infoProp}>Pontos de Situação: </span>
+                <FormGroup>
+                    <FormControlLabel
+                        style={styles.formControlCheckbox}
+                        control={<Checkbox checked={pontosSituacao.curso} onChange={handleCheckboxChange} name="curso" />}
+                        label="Curso (Ativo)"
+                    />
+                    <FormControlLabel
+                        style={styles.formControlCheckbox}
+                        control={<Checkbox checked={pontosSituacao.resolucao} onChange={handleCheckboxChange} name="resolucao" />}
+                        label="Resolução (Dominado)"
+                    />
+                    <FormControlLabel
+                        style={styles.formControlCheckbox}
+                        control={<Checkbox checked={pontosSituacao.conclusao} onChange={handleCheckboxChange} name="conclusao" />}
+                        label="Conclusão (Rescaldo)"
+                    />
+                    <FormControlLabel
+                        style={styles.formControlCheckbox}
+                        control={<Checkbox checked={pontosSituacao.finalizado} onChange={handleCheckboxChange} name="finalizado" />}
+                        label="Finalizado (Extinto)"
+                    />
+                </FormGroup>
+            </div>
+
+            <div style={styles.rowInfo}>
+                <span style={styles.infoProp}>Em Veículo: </span>
+                <FormGroup>
+                    <FormControlLabel
+                        style={styles.formControlCheckbox}
+                        control={<Checkbox checked={emVeiculo.rodoviario_ligeiro} onChange={handleEmVeiculoChange} name="rodoviario_ligeiro" />}
+                        label="Veículo Ligeiro"
+                    />
+                    <FormControlLabel
+                        style={styles.formControlCheckbox}
+                        control={<Checkbox checked={emVeiculo.rodoviarioLigeiroMercadorias} onChange={handleEmVeiculoChange} name="rodoviarioLigeiroMercadorias" />}
+                        label="Veículo Ligeiro Mercadorias"
+                    />
+                    <FormControlLabel
+                        style={styles.formControlCheckbox}
+                        control={<Checkbox checked={emVeiculo.rodoviarioPesadoMercadorias} onChange={handleEmVeiculoChange} name="rodoviarioPesadoMercadorias" />}
+                        label="Veículo Pesado de Mercadorias"
+                    />
+                    <FormControlLabel
+                        style={styles.formControlCheckbox}
+                        control={<Checkbox checked={emVeiculo.rodoviarioPesadosPassageiros} onChange={handleEmVeiculoChange} name="rodoviarioPesadosPassageiros" />}
+                        label="Veículo Pesado de Passageiros"
+                    />
+                    <FormControlLabel
+                        style={styles.formControlCheckbox}
+                        control={<Checkbox checked={emVeiculo.rodoviarioOutros} onChange={handleEmVeiculoChange} name="rodoviarioOutros" />}
+                        label="Outros"
+                    />
+                </FormGroup>
+            </div>
+
+            <div style={styles.rowInfo}>
+                <FormGroup>
+                    <div style={styles.solicitoLabel}>Tipo de Carga:</div>
+                    <TextField
+                        style={styles.solicitoInputDesc}
+                        variant="outlined"
+                        fullWidth
+                        value={tipoCarga.tipoCarga}
+                        onChange={(e) => handleTipoCargaChange('tipoCarga', e.target.value)}
+                    />
+                </FormGroup>
+            </div>
+
+        </div>
     );
 };
 
@@ -47,7 +173,36 @@ const styles = {
     },
     formControlCheckbox: {
         paddingLeft: ' 20px'
-    }
+    },
+    solicitoContainer: {
+        display: 'flex',
+        flexDirection: 'column', // Changed to column to stack rows vertically
+        alignItems: 'flex-start', // Adjust alignment for the column layout
+        padding: '10px',
+        border: '1px solid #ccc',
+        borderRadius: '8px',
+        marginTop: 25,
+        width: '100%', // Ensures full width of container
+    },
+    solicitoRow: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: '100%',
+        marginBottom: '15px', // Adds spacing between rows
+    },
+    solicitoLabel: {
+        fontWeight: 'bold',
+        marginRight: '10px',
+    },
+    solicitoInputQt: {
+        width: '60px',
+        marginRight: '20px',
+    },
+    solicitoInputDesc: {
+        flex: 1,
+        marginTop: "15px"
+    },
 };
 
 export default IncendiosRodoviariosComponent;
